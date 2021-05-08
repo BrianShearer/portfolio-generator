f7500a40d866ed52ffd5e165f82c19832a11775b
Brian Shearer <rrckhunter7@gmail.com>
2021-05-08 13:46:36 -0600

    script start

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 
-console.log('Hello Node!');
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+// var profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs); 
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+
+
+
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+
+    profileDataArr.forEach((profileItem) => {
+        console.log(profileItem)
+      });
+    };
+
+  
+  printProfileData(profileDataArgs);
