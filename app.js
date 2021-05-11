c0e68fa8868d8aac0d5551adefc6b815c3a99eb9
Brian Shearer <rrckhunter7@gmail.com>
2021-05-11 17:11:30 -0600

    finished html

--- app.js
+++ app.js
@@ -128,15 +128,15 @@ Add a New Project
     });
 };
 
-promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    const pageHTML = generatePage();
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     const pageHTML = generatePage(portfolioData);
 
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
     
-  });
+// });
+
+const pageHTML = generatePage(mockData);
